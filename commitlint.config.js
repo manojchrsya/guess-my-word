a91b5414a980e44ecaa57108f3aee4f0811778de
MANOJ CHAURASIYA-ADPL <manojchrsya@gmail.com>
2021-02-20 12:57:31 +0530

    init: initial commit

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,9 @@
+const { rules } = require('@commitlint/config-conventional')
+const enumRules = rules['type-enum'][2].push('init');
+
 module.exports = {
-    extends: ['@commitlint/config-conventional']
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': enumRules
+  },
 };
